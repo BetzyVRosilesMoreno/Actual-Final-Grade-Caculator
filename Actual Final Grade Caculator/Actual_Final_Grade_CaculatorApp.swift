86de4df6ca6e183c88ad6a95ce914ffa93325a42
Betzy V. Rosiles Moreno <brosilesmoreno@bsd220.org>
2023-10-24 09:46:47 -0500

    Initial Commit

---
+++ Actual Final Grade Caculator/Actual_Final_Grade_CaculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Actual_Final_Grade_CaculatorApp.swift
+//  Actual Final Grade Caculator
+//
+//  Created by Betzy Moreno on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Actual_Final_Grade_CaculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
